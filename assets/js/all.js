339b8da11eceb0a0b04a5c66e2a745a4d23a9634
CHUN-CHIA <a121515222@gmail.com>
2022-08-02 12:20:33 +0800

    Update 2022-08-02T04:20:21.402Z

---
+++ assets/js/all.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var hamburger = document.querySelector('.hamburger');
+var nav = document.querySelector('.nav');
+var headerlink = document.querySelectorAll('.nav li a');
+var pagenation = document.querySelectorAll('.pagenation li');
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburgerActive');
+  nav.classList.toggle('navActive');
+});
+pagenation.forEach(function (item, index) {
+  item.addEventListener('click', function (e) {
+    var _this = this;
+
+    e.preventDefault();
+
+    if (index === 0 || index === pagenation.length - 1) {
+      return;
+    } else {
+      pagenation.forEach(function (j, k) {
+        if (Number(_this.firstChild.innerText) === k) {
+          _this.classList.add('pagenationFocus');
+
+          console.log('move');
+        } else {
+          console.log('test');
+          j.classList.remove('pagenationFocus');
+        }
+      });
+    }
+  });
+});
+//# sourceMappingURL=all.js.map
